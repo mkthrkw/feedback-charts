8eda3a73fe42e3b3a08358595937b200d62f891f
mkthrkw <mkt1536@gmail.com>
2025-01-12 20:20:17 +0900

    Add: actions file

---
+++ db/actions.ts
@@ -0,0 +1,97 @@
+"use server";
+import { prisma } from "@/prisma/prisma";
+import type { Feedback } from "@prisma/client";
+import type { CategoryCount, DailyCount, SentimentCount } from "./type";
+
+/**
+ * 指定されたページとページサイズに基づいてフィードバックアイテムを取得します。
+ * @param {number} page - 取得するページの番号（1から始まる）。
+ * @param {number} pageSize - 1ページあたりのアイテム数。
+ * @returns {Promise<Feedback[]>} フィードバックアイテムの配列を返します。
+ */
+export const getFeedbackItems = async ({
+	page,
+	pageSize,
+}: {
+	page: number;
+	pageSize: number;
+}): Promise<Feedback[]> => {
+	const feedbacks = await prisma.feedback.findMany({
+		skip: (page - 1) * pageSize,
+		take: pageSize,
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
+	return feedbacks;
+};
+
+/**
+ * 指定された期間内の日ごとのフィードバック数を取得します。
+ * @param {Date} from - 集計を開始する日付。
+ * @param {Date} to - 集計を終了する日付。
+ * @returns {Promise<DailyCount[]>} 日ごとのフィードバック数を含むオブジェクトの配列を返します。
+ */
+export const getDailyCounts = async ({
+	from,
+	to,
+}: {
+	from: Date;
+	to: Date;
+}): Promise<DailyCount[]> => {
+	const dailyCounts = await prisma.$queryRaw`
+		SELECT DATE("createdAt") as date, COUNT(*)::int as count
+		FROM "Feedback"
+		WHERE "createdAt" BETWEEN ${from} AND ${to}
+		GROUP BY DATE("createdAt")
+		ORDER BY date ASC;
+	`;
+	console.log(dailyCounts);
+	return dailyCounts as DailyCount[];
+};
+
+/**
+ * 指定された期間内のカテゴリごとのフィードバック数を取得します。
+ * @param {Date} from - 集計を開始する日付。
+ * @param {Date} to - 集計を終了する日付。
+ * @returns {Promise<CategoryCount[]>} カテゴリごとのフィードバック数を含むオブジェクトの配列を返します。
+ */
+export const getCategoryCounts = async ({
+	from,
+	to,
+}: {
+	from: Date;
+	to: Date;
+}): Promise<CategoryCount[]> => {
+	const categoryCounts = await prisma.$queryRaw`
+		SELECT "category", COUNT(*)::int as count
+		FROM "Feedback"
+		WHERE "createdAt" BETWEEN ${from} AND ${to}
+		GROUP BY "category"
+		ORDER BY category ASC;
+	`;
+	return categoryCounts as CategoryCount[];
+};
+
+/**
+ * 指定された期間内の感情ごとのフィードバック数を取得します。
+ * @param {Date} from - 集計を開始する日付。
+ * @param {Date} to - 集計を終了する日付。
+ * @returns {Promise<SentimentCount[]>} 感情ごとのフィードバック数を含むオブジェクトの配列を返します。
+ */
+export const getSentimentCounts = async ({
+	from,
+	to,
+}: {
+	from: Date;
+	to: Date;
+}): Promise<SentimentCount[]> => {
+	const sentimentCounts = await prisma.$queryRaw`
+		SELECT "sentiment", COUNT(*)::int as count
+		FROM "Feedback"
+		WHERE "createdAt" BETWEEN ${from} AND ${to}
+		GROUP BY "sentiment"
+		ORDER BY sentiment ASC;
+	`;
+	return sentimentCounts as SentimentCount[];
+};
---
+++ db/type.ts
@@ -0,0 +1,14 @@
+export type DailyCount = {
+	date: Date;
+	count: number;
+};
+
+export type CategoryCount = {
+	category: string;
+	count: number;
+};
+
+export type SentimentCount = {
+	sentiment: string;
+	count: number;
+};
