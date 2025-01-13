09183815e0f05577c860a52eac61d1bcc1849d92
mkthrkw <mkt1536@gmail.com>
2025-01-14 00:18:22 +0900

    Update: ページコンポーネント更新 - ダッシュボードページのコンポーネントを更新 - サマリーカードコンポーネントをダッシュボードページに追加 - 年代別と評価別チャートコンポーネントをダッシュボードページに追加 - データ取得関数を更新 - ページディレクトリ内にcomponentsディレクトリを作成しファイル移動

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
+import { BarChartComponent } from "@/components/charts/bar-chart";
 import { LineChartComponent } from "@/components/charts/line-chart";
 import { PieChartComponent } from "@/components/charts/pie-chart";
 import { RadarChartComponent } from "@/components/charts/radar-chart";
+import { RadialChartComponent } from "@/components/charts/radial-chart";
 import { PageHeader } from "@/components/layouts/page-header";
-import { getCategoryCounts, getDailyCounts, getSentimentCounts } from "@/db/actions";
-import { endOfDay, format, startOfDay, subDays } from "date-fns";
-import { DateRangePicker } from "./date-range-picker";
+import { getCategoryCounts, getDailyCountsAndRating, getGenerationCounts, getRatingCounts, getSentimentCounts } from "@/db/actions";
+import { differenceInDays, endOfDay, format, startOfDay, subDays } from "date-fns";
+import { DateRangePicker } from "./components/date-range-picker";
+import { SummaryCards } from "./components/summary-cards";
 
 export default async function Page(props: {
   searchParams: Promise<{
@@ -18,28 +21,39 @@ export default async function Page(props: {
   const rangeFrom = searchParams?.from ? startOfDay(new Date(searchParams.from)) : defaultFrom;
   const rangeTo = searchParams?.to ? endOfDay(new Date(searchParams.to)) : defaultTo;
 
-  const dailyCounts = await getDailyCounts({ rangeFrom: rangeFrom, rangeTo: rangeTo });
-  const categoryCounts = await getCategoryCounts({ rangeFrom: rangeFrom, rangeTo: rangeTo });
-  const sentimentCounts = await getSentimentCounts({ rangeFrom: rangeFrom, rangeTo: rangeTo });
+  const [dailyCountsAndRating, categoryCounts, sentimentCounts, generationCounts, ratingCounts] = await Promise.all([
+    getDailyCountsAndRating({ rangeFrom: rangeFrom, rangeTo: rangeTo }),
+    getCategoryCounts({ rangeFrom: rangeFrom, rangeTo: rangeTo }),
+    getSentimentCounts({ rangeFrom: rangeFrom, rangeTo: rangeTo }),
+    getGenerationCounts({ rangeFrom: rangeFrom, rangeTo: rangeTo }),
+    getRatingCounts({ rangeFrom: rangeFrom, rangeTo: rangeTo }),
+  ]);
+
+  const totalCount = dailyCountsAndRating.reduce((acc, curr) => acc + curr.count, 0);
+  const dailyAverageCount = Math.round(totalCount / dailyCountsAndRating.length);
+  const totalAverageRating = dailyCountsAndRating.reduce((acc, curr) => acc + curr.rating, 0);
+  const dailyAverageRating = (totalAverageRating / dailyCountsAndRating.length).toFixed(2);
+  const rangeDays = differenceInDays(rangeTo, rangeFrom);
 
   const rangeFromString = format(rangeFrom, "yyyy-MM-dd HH:mm:ss");
   const rangeToString = format(rangeTo, "yyyy-MM-dd HH:mm:ss");
 
-  console.log(rangeFromString, rangeToString);
-
   return (
     <div className="flex flex-col gap-4 w-full">
       <PageHeader title="DashBoard" />
-      <main className="flex flex-col gap-4 p-4 pt-0 container mx-auto">
+      <main className="flex flex-col gap-2 sm:gap-4 p-4 pt-0 container max-w-6xl mx-auto">
         <div className="flex justify-end">
           <DateRangePicker rangeFromString={rangeFromString} rangeToString={rangeToString} />
         </div>
-        <div className="flex flex-wrap lg:flex-nowrap gap-4">
-          <LineChartComponent chartData={dailyCounts} className="w-full" />
-        </div>
+        <SummaryCards totalCount={String(totalCount)} dailyAverageCount={String(dailyAverageCount)} dailyAverageRating={String(dailyAverageRating)} rangeDays={String(rangeDays)} />
+        <LineChartComponent chartData={dailyCountsAndRating} className="w-full" />
         <div className="flex flex-wrap lg:flex-nowrap gap-4">
           <PieChartComponent chartData={categoryCounts} className="w-full" />
           <RadarChartComponent chartData={sentimentCounts} className="w-full" />
+        </div>
+        <div className="flex flex-wrap lg:flex-nowrap gap-4">
+          <BarChartComponent chartData={generationCounts} className="w-full" />
+          <RadialChartComponent chartData={ratingCounts} className="w-full" />
         </div>
       </main>
     </div>
